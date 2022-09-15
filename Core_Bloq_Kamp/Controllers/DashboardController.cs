f1222ac99cd33ef14d9114383754447b64caf4de
MDNET-Developer <qarabagli.murad@mail.ru>
2022-09-15 14:07:51 +0400

    new dashboard page

---
+++ Core_Bloq_Kamp/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Core_Bloq_Kamp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            Context db = new Context();
+            ViewBag.blog = db.Blogs.Where(x => x.WriterID == 1).Count();
+
+            return View();
+        }
+    }
+}
---
+++ Core_Bloq_Kamp/ViewComponents/GetWriterInfoOnDashboard.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core_Bloq_Kamp.ViewComponents
+{
+    public class GetWriterInfoOnDashboard : ViewComponent
+    {
+
+        WriterManager writerManager = new WriterManager(new EfWriterRepository());
+        public IViewComponentResult Invoke()
+        {
+            var userinfo = writerManager.GetWriterAccountByID(1);
+            return View(userinfo);
+        }
+    }
+}
